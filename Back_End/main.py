cc7f6aac3f671f31e3832fcafdac82252a6eda40
Euijin Jung <ejung@angelo.edu>
2026-02-17 00:23:18 -0600

    Login Verification w/ MySQL DB

--- Back_End/main.py
+++ Back_End/main.py
@@ -1,19 +1,23 @@
-### download fastapi library and uvicorn tool using the command below in terminal
-# pip install "fastapi[standard]"
-
-### At ./SWE-Project, run the command below in terminal
-# uvicorn Back_End.main:app --reload
-
-### Then put this url or "Open with Live Server"
-# http://127.0.0.1:5500/Front_End/index123456.html
+import uuid
+import datetime
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from Back_End.login_verification import check_id_password
+from Back_End import mysqlconnector
 
+while True:
+    connection, db = mysqlconnector.connectSQL()
+    if connection == True:
+        cursor = db.cursor()
+        break
+    else:
+        print("Not responsding...")
+
+# Create App
 app = FastAPI()
 
+# Allows front end connection to this API
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  
@@ -22,35 +26,55 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def welcomePage():
-    return {
-        "message": "Where all starts..."
-    }
-
-@app.get("/welcome/")
+# http://localhost:8000/api/welcome/
+@app.get("/api/welcome/")
 def welcomePage():
     return {
         "message": "welcome"
     }
 
-@app.get("/welcome/login_page/")
-def welcomePage():
-    return {
-        "message": "Please enter your ID and Password!!!!"
-    }
+# http://localhost:8000/api/login/?id=put_id_here&pw=put_pw_here
+@app.get("/api/login/")
+def login_result(id: str, pw: str):
+    try:
+        # Get u_uuid and first name from user_priv_info DB if id and pw matches
+        cursor.execute("SELECT u_uuid, first_name FROM user_priv_info WHERE user_id = %s AND user_pw = %s", (id, pw))
+        rows = cursor.fetchone()
+        user_uuid, user_first_name = rows[0], rows[1]
 
-@app.get("/welcome/login_page/result/")
-def welcomePage(id: str, pw: str):
-    if check_id_password(id, pw):
-        return {
-            "message": f"Welcome! {id}!!",
-            "id": {id},
-            "password": {pw}
-        }
-    else:
-        return {
-            "message": "You cant get in lolol",
-            "what you put": f"id: {id}, pw: {pw}",
-            "Failure": "user info do not match"
-        }
+        # Make status message
+        login_log = "User Login: " + user_uuid
+
+        # Get uuid for the login attempt & Get time when user attempted loging in
+        login_attempt_uuid, login_time = str(uuid.uuid4()), datetime.datetime.now()
+
+        # Post login attempt to login_attempts DB
+        cursor.execute(
+        "INSERT INTO login_attempts (la_uuid, datetime_sent, typed_id, typed_pw, status) " \
+            "VALUES (%s, %s, %s, %s, %s)", (login_attempt_uuid, login_time, id, pw, login_log))
+
+        # Save changes in login_attempts DB
+        db.commit()
+
+        # Return value
+        return {"message": f"Welcome! {user_first_name}!"}
+
+    # If anything went wrong inside try, then this part will be run
+    except Exception as e:
+        print("ERROR:", e)
+        # Make status message
+        login_log = "Failed Login: Incorrect ID or Password"
+
+        # Get uuid for the login attempt & Get time when user attempted loging in
+        login_attempt_uuid, login_time = str(uuid.uuid4()), datetime.datetime.now()
+
+        # Post login attempt to login_attempts DB
+        cursor.execute("" \
+        "INSERT INTO login_attempts (la_uuid, datetime_sent, typed_id, typed_pw, status) " \
+            "VALUES (%s, %s, %s, %s, %s)", (login_attempt_uuid, login_time, id, pw, login_log))
+
+        # Save changes in login_attempts DB
+        db.commit()
+
+        # Return value
+        return {"message": "Login Failed. Try Again!"}
