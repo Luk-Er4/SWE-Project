9bbd23ab5748cf1bd52a8cd0e4514918fed1f006
Euijin Jung <ejung@angelo.edu>
2026-03-26 02:16:19 -0500

    login_requirements & create users

--- Back_End/main.py
+++ Back_End/main.py
@@ -4,11 +4,11 @@ import datetime
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from Back_End import mysqlconnector
+from Back_End import login_requirements, mysqlconnector
 
 while True:
     connection, db = mysqlconnector.connectSQL()
-    if connection == True:
+    if connection == True:  
         cursor = db.cursor()
         break
     else:
@@ -34,7 +34,7 @@ def welcomePage():
     }
 
 # http://localhost:8000/api/login/?id=cherydought8&pw=\Er8oX6u9t
-@app.get("/api/login/")
+@app.post("/api/login/")
 def login_result(id: str, pw: str):
     try:
         # Get u_uuid and first name from user_priv_info DB if id and pw matches
@@ -71,11 +71,58 @@ def login_result(id: str, pw: str):
         # Post login attempt to login_attempts DB
         cursor.execute("" \
         "INSERT INTO login_attempts (la_uuid, datetime_sent, typed_id, typed_pw, status) " \
-            "VALUES (%s, %s, %s, %s, %s)", (login_attempt_uuid, login_time, id, pw, login_log))
+            "VALUES (%s, %s, %s, %s, %s)", (login_attempt_uuid, login_time, id, None, login_log))
 
         # Save changes in login_attempts DB
         db.commit()
 
         # Return value
         return {"message": "Login Failed. Try Again!"}
+
+# http://localhost:8000/api/new_user/?first=ABCDE&last=FGHIJ&id=emahogdf452&pw=p@as$vv0rD
+@app.post("/api/new_user/")
+def login_result(first: str, last:str, id: str, pw: str):
+    '''
+    1. Check if there is at least one of inputs has invalid inputs causing SQL injection
+    2. Check id and password requirements
+    3. Then add user info to table
+    '''
+    try:
+        ## Step 1 ##
+        # Retrieve all column names in table user_priv_info
+        not_allowed_names = login_requirements.checkcolnames(cursor)
+
+        # Any inputs named like "first_name", then throw an error
+        for var in [first, last, id, pw]:
+            if var in not_allowed_names:
+                raise ValueError(f"Sorry, there are one or more invalid identifiers")
         
+        ## Step 2 ##
+        id_issue = login_requirements.id_requirements(cursor, id)
+
+        if id_issue is not None: 
+            raise ValueError(id_issue)
+
+        pw_issue = login_requirements.pw_requirements(pw)
+
+        if pw_issue is not None:
+            raise ValueError(pw_issue)
+
+        ## Step 3 ##
+        user_priv_uuid, created_date = str(uuid.uuid4()), datetime.datetime.now()
+
+        # Take name, id, and password, then put it to user_priv_info DB
+        cursor.execute("" \
+        "INSERT INTO user_priv_info (u_uuid, first_name, last_name, user_id, user_pw, created_at) " \
+            "VALUES (%s, %s, %s, %s, %s, %s)", (user_priv_uuid, first, last, id, pw, created_date))
+        
+        # Save changes in login_attempts DB
+        db.commit()
+
+        # Return value
+        return {"message": f"Welcome! {first} {last}!"}
+
+    # If anything went wrong inside try, then this part will be run
+    except Exception as e:
+        # Return value
+        return {"message": f"{e}"}
