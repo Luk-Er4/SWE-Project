d74d72051f776d82bfe2db78e44bb8f3199ed81d
Euijin Jung <ejung@angelo.edu>
2026-04-26 03:42:06 -0500

    Ready to Assign Task

--- health-scoring-frontend/src/Pages/Landingpg.jsx
+++
@@ -1,9 +0,0 @@
-import LandingPage from "../Components/LandingPage";
-
-export default function Landingpg({ onStart }) {
-  return (
-    <div className="page">
-      <LandingPage onStart={onStart} />
-    </div>
-  );
-}
